771b76a4e7be41c1b89690f777ca80280275e5f1
huangguanhua <huanguanhua@qdama.cn>
2021-04-09 16:38:53 +0800

    'init5'

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.scss';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './styles/index.scss';
+// import App from './App';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//     <App />,
+//   document.getElementById('root')
+// );
 
-// import {library} from '@fortawesome/fontawesome-svg-core';
-// import {fas}from '@fortawesome/free-solid-svg-icons';
-// library.add(fas);
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {fas}from '@fortawesome/free-solid-svg-icons';
+library.add(fas);
 
-// export {default as Button} from '../src/components/Button';
-// export {default as Menu} from '../src/components/Menu';
-// export {default as Alert} from '../src/components/Alert';
-// export {default as AutoComplete} from '../src/components/AutoComplete';
-// export {default as Upload} from '../src/components/Upload';
-// export {default as Icon} from '../src/components/Icon';
-// export {default as Input} from '../src/components/Input';
-// export {default as Progress} from '../src/components/Progress';
-// export {default as Transition} from '../src/components/Transition';
+export {default as Button} from '../src/components/Button';
+export {default as Menu} from '../src/components/Menu';
+export {default as Alert} from '../src/components/Alert';
+export {default as AutoComplete} from '../src/components/AutoComplete';
+export {default as Upload} from '../src/components/Upload';
+export {default as Icon} from '../src/components/Icon';
+export {default as Input} from '../src/components/Input';
+export {default as Progress} from '../src/components/Progress';
+export {default as Transition} from '../src/components/Transition';
